a576711abd9fa9f1519795074bf290d7f6af5dbb
Razboy20 <razboy20@gmail.com>
2024-09-22 00:55:32 -0500

    feat: add umami analytics

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -12,6 +12,11 @@ export default createHandler((ctx) => {
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href={process.env.VITE_BASE_URL + "favicon.ico"} />
+            <script
+              defer
+              src={process.env.VITE_BASE_URL + "uscript.js"}
+              data-website-id="50f3c21b-d118-4869-9b8f-ad47c1cea369"
+            ></script>
             {assets}
           </head>
           <body class="font-sans min-h-screen bg-neutral-50 transition-colors duration-100 dark:bg-neutral-900 dark:text-neutral-100 text-neutral-950">
